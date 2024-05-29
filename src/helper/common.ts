269ce50c590840110ef82f165b5c4b50231ccaab
Gubendiran Moorthy <GMoorthy@corp.encoress.com>
2024-05-29 15:20:45 +0530

    changed date

--- src/helper/common.ts
+++ src/helper/common.ts
@@ -64,7 +64,7 @@ export class CommonController {
                 case 1: day = "Monday"; break;
                 case 2: day = "Tuesday"; break;
                 case 3: day = "Wednesday"; break;
-                case 4: day = "Thurday"; break;
+                case 4: day = "Thursday"; break;
                 case 5: day = "Friday"; break;
                 case 6: day = "Saturday"; break;
             }
