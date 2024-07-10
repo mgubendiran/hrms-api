b92e079ad8efaaedad955fee581f78e23a0624ed
Gubendiran Moorthy <GMoorthy@corp.encoress.com>
2024-07-10 08:08:44 +0530

    log updates

--- src/models/AttendenceLog.ts
+++ src/models/AttendenceLog.ts
@@ -15,11 +15,39 @@ export interface AttendanceLogInterface {
 	Status: string;
 	StatusCode: string;
     day: string;
+    PunchRecords: string,
+    InTime: string,
+    InDeviceId: string,
+    OutTime: string,
+    OutDeviceId: string,
+    Duration: number,
+    LateBy: number,
+    EarlyBy: number,
+    ShiftId: number
 }
 
 // Sequelize Model
 export const AttendanceLog: sequelize.Model<AttendanceLogInterface, {}> = databaseInstance.define<AttendanceLogInterface, {}>("AttendanceLogs", {
-       
+
+// CREATE TABLE attendance.dbo.AttendanceLog (
+// 	AttendanceLogID int NOT NULL,
+// 	AttendanceDate datetime NULL,
+// 	Present bit NULL,
+// 	Absent bit NULL,
+// 	Status varchar(100) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	StatusCode varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	PunchRecords text COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	InTime varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	InDeviceId varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	OutTime varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	OutDeviceId varchar(255) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	Duration float NULL,
+// 	LateBy bigint NULL,
+// 	EarlyBy bigint NULL,
+// 	ShiftId bigint NULL,
+// 	EmployeeNumber varchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+// 	EmployeeName varchar(200) COLLATE SQL_Latin1_General_CP1_CI_AS NULL
+// );
     AttendanceLogID: {
         type: sequelize.INTEGER,
         autoIncrement: true,
@@ -51,6 +79,42 @@ export const AttendanceLog: sequelize.Model<AttendanceLogInterface, {}> = databa
     },
     StatusCode: {
         type: sequelize.STRING,
+        allowNull: true
+    },
+    PunchRecords: {
+        type: sequelize.TEXT,
+        allowNull: true
+    },
+    InTime: {
+        type: sequelize.STRING,
+        allowNull: true
+    },
+    InDeviceId: {
+        type: sequelize.STRING,
+        allowNull: true
+    },
+    OutTime: {
+        type: sequelize.STRING,
+        allowNull: true
+    },
+    OutDeviceId: {
+        type: sequelize.STRING,
+        allowNull: true
+    },
+    Duration: {
+        type: sequelize.FLOAT,
+        allowNull: true
+    },
+    LateBy: {
+        type: sequelize.BIGINT,
+        allowNull: true
+    },
+    EarlyBy: {
+        type: sequelize.BIGINT,
+        allowNull: true
+    },
+    ShiftId: {
+        type: sequelize.BIGINT,
         allowNull: true
     }
 }, {
